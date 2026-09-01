6df67f3f810b411a5d460ea2f3e13ff93eed6352
kamal eddine <kamalden89@gmail.com>
2026-09-02 01:11:31 +0300

    Add first Java program - variables and println

---
+++ phase1-console-app/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String customerName = "Kamal";
+        double balance = 500.0;
+        boolean isActive = true;
+        System.out.println("Customer: " + customerName);
+        System.out.println("Balance: " + balance);
+    }
+}
